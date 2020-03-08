b085035ce2980052300dbe4eda53b08872b91944
Chris Arthurs <christopher.arthurs@kcl.ac.uk>
2020-03-08 17:09:36 +0000

    Changed to have just a few default places shown when the graph is first rendered

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ import pathlib
 import plotly.graph_objects as go
 
 class Configuration(object):
-    def __init__(self, full_datafile_path, include_georegions_with_at_least_this_many_cases, logplot, differential_plot):
+    def __init__(self, full_datafile_path, include_georegions_with_at_least_this_many_cases, logplot,
+                 differential_plot, initial_data_to_show):
         self.full_datafile_path = full_datafile_path
         self.include_georegions_with_at_least_this_many_cases = include_georegions_with_at_least_this_many_cases
         self.logplot = logplot
         self.differential_plot = differential_plot
+        self.initial_data_to_show = initial_data_to_show
 
 def run_plotting(config):
     plot_all = True
@@ -72,9 +74,15 @@ def run_plotting(config):
             else:
                 y_data = slice_of_data_for_plotting.loc[data_index, :]
 
+            if country in config.initial_data_to_show or province_or_state in config.initial_data_to_show:
+                visibility_setting = None
+            else:
+                visibility_setting = 'legendonly'
+
             fig.add_trace(go.Scatter(y=y_data,
                                      x=list(data)[4: -1],
                                      name=trace_name,
+                                     visible=visibility_setting,
                                      hoverlabel=dict(namelength=-1, bgcolor='rgba(188, 20, 26, 0.5)')))
 
         fig.update_layout(title=plot_title,
@@ -95,7 +103,8 @@ if __name__ == '__main__':
     file_name = r'time_series_19-covid-Confirmed.csv'
     full_datafile_path = data_dir / file_name
     include_georegions_with_at_least_this_many_cases = 100
+    initial_data_to_show = ['UK', 'Italy', 'Germany', 'Taiwan', 'Iran', 'USA', 'Hubei']
 
     for logplot in [True, False]:
         for differential_plot in [True, False]:
-            run_plotting(Configuration(full_datafile_path, include_georegions_with_at_least_this_many_cases, logplot, differential_plot))
+            run_plotting(Configuration(full_datafile_path, include_georegions_with_at_least_this_many_cases, logplot, differential_plot, initial_data_to_show))
