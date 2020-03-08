1289e51cb1260f6929286a83a7bf8c5cc900169b
Chris Arthurs <christopher.arthurs@kcl.ac.uk>
2020-03-08 16:03:13 +0000

    Added log and differential plotting options

--- main.py
+++ main.py
@@ -7,25 +7,53 @@ import pandas
 import pathlib
 import plotly.graph_objects as go
 
-if __name__ == '__main__':
-    data_dir = pathlib.Path(r'/home/chris/WorkData/covid19/COVID-19/csse_covid_19_data/csse_covid_19_time_series')
-    file_name = r'time_series_19-covid-Confirmed.csv'
-    include_georegions_with_at_least_this_many_cases = 100
-    plot_all = True
+class Configuration(object):
+    def __init__(self, full_datafile_path, include_georegions_with_at_least_this_many_cases, logplot, differential_plot):
+        self.full_datafile_path = full_datafile_path
+        self.include_georegions_with_at_least_this_many_cases = include_georegions_with_at_least_this_many_cases
+        self.logplot = logplot
+        self.differential_plot = differential_plot
 
-    data = pandas.read_csv(data_dir / file_name)
+def run_plotting(config):
+    plot_all = True
+    if config.logplot:
+        y_axis_type = 'log'
+    else:
+        y_axis_type = 'linear'
+
+    data = pandas.read_csv(config.full_datafile_path)
     last_available_data_date = list(data)[-1]
     first_available_data_date = list(data)[4]
+
     slice_of_data_for_plotting = data.loc[:, first_available_data_date:last_available_data_date]
+    # differential_data = slice_of_data_for_plotting.copy(deep=True)
+    #
+    # print(differential_data)
+    # for row in range(differential_data.shape[0]):
+    #     for column in range(differential_data.shape[1]):p
+
 
     print(list(data))
     print(list(data.loc[:, 'Province/State']))
 
     indices_of_included_georegions = [data_index for data_index, country_or_region in
                                       enumerate(list(data.loc[:, 'Country/Region'])) if
-                                      data.loc[data_index, last_available_data_date] > include_georegions_with_at_least_this_many_cases]
+                                      data.loc[data_index, last_available_data_date] > config.include_georegions_with_at_least_this_many_cases]
 
     print("number of graphs to plot:", len(indices_of_included_georegions))
+
+    plot_title = 'COVID-19 Cases by Country (Those with At Least {} Cases)'.format(config.include_georegions_with_at_least_this_many_cases)
+
+    if config.logplot:
+        plot_title = 'Logarithmic ' + plot_title
+    else:
+        plot_title = 'Linear ' + plot_title
+
+    if config.differential_plot:
+        plot_title = 'Differential ' + plot_title
+    else:
+        plot_title = 'Cumulative ' + plot_title
+
 
     if plot_all:
         fig = go.Figure()
@@ -39,13 +67,35 @@ if __name__ == '__main__':
                 trace_name = country
 
             print('Plotting {} - Region index {} - {}'.format(loop_index, data_index, trace_name))
-            fig.add_trace(go.Scatter(y=slice_of_data_for_plotting.loc[data_index, :],
+            if config.differential_plot:
+                y_data = slice_of_data_for_plotting.loc[data_index, :].diff()
+            else:
+                y_data = slice_of_data_for_plotting.loc[data_index, :]
+
+            fig.add_trace(go.Scatter(y=y_data,
                                      x=list(data)[4: -1],
                                      name=trace_name,
                                      hoverlabel=dict(namelength=-1, bgcolor='rgba(188, 20, 26, 0.5)')))
 
-        fig.update_layout(title='Cumulative COVID-19 Cases by Country (Those with At Least {} Cases)'.format(include_georegions_with_at_least_this_many_cases),
+        fig.update_layout(title=plot_title,
                           xaxis_title='Date',
-                          yaxis_title='Cumulative Cases')
+                          yaxis_title='Cumulative Cases',
+                          yaxis_type=y_axis_type)
         # fig.show()
-        plotly.offline.plot(fig, filename='/home/chris/covid.html')
+
+        output_filename = 'covid'
+        if config.logplot:
+            output_filename += '_logplot'
+        if config.differential_plot:
+            output_filename += '_diff'
+        plotly.offline.plot(fig, filename='/home/chris/{}.html'.format(output_filename))
+
+if __name__ == '__main__':
+    data_dir = pathlib.Path(r'/home/chris/WorkData/covid19/COVID-19/csse_covid_19_data/csse_covid_19_time_series')
+    file_name = r'time_series_19-covid-Confirmed.csv'
+    full_datafile_path = data_dir / file_name
+    include_georegions_with_at_least_this_many_cases = 100
+
+    for logplot in [True, False]:
+        for differential_plot in [True, False]:
+            run_plotting(Configuration(full_datafile_path, include_georegions_with_at_least_this_many_cases, logplot, differential_plot))
